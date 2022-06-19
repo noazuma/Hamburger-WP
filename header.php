b20057b7367703adc2f235397aa8784af8f43a97
kazuki higashi <kazukihigashi@kazukinoMacBook-Air.local>
2022-06-19 23:08:29 +0900

    php化化

---
+++ header.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Hamburger</title>
+
+<!-- フォントRoboto -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@700&display=swap" rel="stylesheet">
+
+
+<!--  -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Noto+Sans+JP:wght@300&display=swap" rel="stylesheet">
+<!-- スタイルシート -->
+    <link rel="stylesheet" href="css/reset.css">
+    <link rel="stylesheet" href="style.css">
+    <script src="https://kit.fontawesome.com/21e9ab169d.js" crossorigin="anonymous"></script>
+        
+
+</head>
+<body>
+    <div class="c-cover"></div>
+
+<!--ヘッダー  -->
+<header class="l-header">
+<h2 class="p-head__menu c-button--menu">Menu</h2>
+
+<h1 class="p-head__title c-title"><a href="#">Hamburger</a></h1>
+<serach-box class="p-serach">
+    
+        <input type="search" name="search"  style="padding-left: 30px;" value="" class="p-serach__box c-icon">
+    
+        <input type="submit" value="検索" class="c-button--submit"></input>
+</form>
+    <!-- <a>検索</a> -->
+    </serach-box>
+</header>
