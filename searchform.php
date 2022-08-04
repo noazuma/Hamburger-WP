da857719dd74c26ce72efd6b1361eebcd819f598
kazuki higashi <kazukihigashi@kazukinoMacBook-Air.local>
2022-08-04 22:12:44 +0900

    search form修正

--- searchform.php
+++ searchform.php
@@ -1,7 +1,8 @@
 <!-- 検索フォームを表示 -->
 
 <serach-box class="p-serach" method="get" action="<?php echo home_url('/'); ?>">
-    <input type="search" name="search"  style="padding-left: 30px;" value="<?php echo get_search_query(); ?>" class="p-serach__box c-icon" name="s" id="s">
+<form>
+    <input type="text" name="s"  style="padding-left: 30px;" value="<?php the_search_query(); ?>" class="p-serach__box c-icon" name="s" id="s">
     <input type="submit" value="検索" class="c-button--submit"></input>
 </form>
 </serach-box>
