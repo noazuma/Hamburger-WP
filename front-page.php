f2666ee90b7a415cab6e7bfa9039860312a0af08
kazuki higashi <kazukihigashi@kazukinoMacBook-Air.local>
2022-08-12 23:48:48 +0900

    front-page修正

--- front-page.php
+++ front-page.php
@@ -9,45 +9,45 @@
 <!--メニュー  -->
 <article class="p-branch c-grid">
         <div class="p-branch--menu p-container__take-out">
-            <h2 class="c-title__sub">Take OUT
+            <h2 class="c-title__sub"><?php
+                $category = get_the_category();
+                $cat_name = $category[1]->cat_name;
+                echo $cat_name;
+            ?>
             </h2>
             <span class="c-line p-branch--menu__line"></span>
                 <div class="c-grid">
                     <div class="p-branch--menu__box">
                         <h3 class="c-title__in">Take OUT</h3><a href="#">
-                        <p class="p-branch__sub">当店のテイクアウトで利用できる商品を掲載しています
-                            当店のテイクアウトで利用できる商品を掲載しています
-                            当店のテイクアウトで利用できる商品を掲載しています
+                        <p class="p-branch__sub"><?php echo category_description(6); ?>
                         </p></a>
                     </div>
                     <div class="p-branch--menu__box">
                         <h3 class="c-title__in">Take OUT</h3><a href="#">
-                        <p class="p-branch__sub">当店のテイクアウトで利用できる商品を掲載しています
-                            当店のテイクアウトで利用できる商品を掲載しています
-                            当店のテイクアウトで利用できる商品を掲載しています
+                        <p class="p-branch__sub"><?php echo category_description(6); ?>
                         </p></a>
                     </div>
                 </div>
         </div>
                 
         <div class="p-branch--menu p-container__eat-in">        
-            <h2 class="c-title__sub">Eat In</h2>
+            <h2 class="c-title__sub">
+            <? $category = get_the_category();
+                $cat_name = $category[0]->cat_name;
+                echo $cat_name;
+            ?>
+            </h2>
             <span class="c-line p-branch--menu__line"></span>
                 <div class="c-grid">
                     <div class="p-branch--menu__box">
                         <h3 class="c-title__in">Eat In</h3><a href="#">
-                        <p class="p-branch__sub">店内でお食事いただけるメニューです店内でお
-                            食事いただけるメニューです
-                            店内でお食事いただけるメニューです店内でお
-                            食事いただけるメニューです
+                        <p class="p-branch__sub"><?php echo category_description(5); ?>
                         </p></a>
                     </div>
                     <div class="p-branch--menu__box">
                         <h3 class="c-title__in">Eat In</h3><a href="#">
-                        <p class="p-branch__sub">店内でお食事いただけるメニューです店内でお
-                            食事いただけるメニューです
-                            店内でお食事いただけるメニューです店内でお
-                            食事いただけるメニューです</p></a>
+                        <p class="p-branch__sub"><?php echo category_description(5); ?>
+                        </p></a>
                     </div>
                 </div>       
 
