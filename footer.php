2fcb9cbd92a47848d2eace6b812b5effcc75c3f7
kazuki higashi <kazukihigashi@kazukinoMacBook-Air.local>
2022-08-23 23:24:10 +0900

    フッター&フロント修正

--- footer.php
+++ footer.php
@@ -1,23 +1,23 @@
-        <footer class="l-footer">
+        <footer class="l-footer p-footer">
             
-            <div>
-                <ul class="p-footer">
+            
+            <div>   
                 <?php
                 wp_nav_menu(
                     array(
                         'container' => 'false', //divタグ削除
                         'theme_location' => 'footer-menu', //表示するメニュー名を指定
-                        'items_wrap' => '<li class="p-footer__shop">%3$s</li>', //li要素のID,classを非表示
+                        'items_wrap' => '<ul class="p-footer__shop" %3$s</ul>', //ul要素のID,classを非表示
                         )
                     );
-            ?>
+                ?>
                     <!-- <li><a class="p-footer__shop" href="#">ショップ情報</a></li>
                     <spen class="c-line--footer p-partition"></spen> 
                     <li><a class="p-footer__shop" href="#">ヒストリー</a></li> -->
-                </ul>
+                
 
-                <p class="p-footer__cp">Copyright: RaiseTech</p>
-            </div>
+            <p class="p-footer__cp">Copyright: RaiseTech</p>
+                </div> 
         </footer>  
         
     <?php get_sidebar(); ?>
